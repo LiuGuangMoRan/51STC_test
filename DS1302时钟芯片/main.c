64a913e2ab2d926426395224f3badf12aae8f937
LiuGuangMoRan <113183353+LiuGuangMoRan@users.noreply.github.com>
2023-03-05 22:28:30 +0800

    Add files via upload

---
+++ DS1302时钟芯片/main.c
@@ -0,0 +1,22 @@
+#include <REGX52.H>
+#include <Tool.h>
+#include <DS1302.h>
+
+void main()
+{
+	LCD_Init();
+	DS1302_Init();
+	LCD_ShowString(1,1,"  -  -  ");//?????????
+	LCD_ShowString(2,1,"  :  :  ");
+	DS1302_SetTime();
+  while(1)
+  {
+    DS1302_ReadTime();//????
+	  LCD_ShowNum(1,1,DS1302_Time[0],2);//???
+	  LCD_ShowNum(1,4,DS1302_Time[1],2);//???
+	  LCD_ShowNum(1,7,DS1302_Time[2],2);//???
+	  LCD_ShowNum(2,1,DS1302_Time[3],2);//???
+	  LCD_ShowNum(2,4,DS1302_Time[4],2);//???
+	  LCD_ShowNum(2,7,DS1302_Time[5],2);//???
+    }
+}
